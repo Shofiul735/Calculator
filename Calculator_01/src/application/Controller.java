97175e6e9cd1aca9d7ffb27002641665f3c55940
Shofiul <shofiul.islam@northsouth.edu>
2018-12-23 00:25:25 +0600

    Add files via upload

---
+++ Calculator_01/src/application/Controller.java
@@ -0,0 +1,45 @@
+package application;
+
+import javafx.scene.control .Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+	@FXML
+	private Label result;
+	private long number1=0;
+	private String operator="";
+	private boolean start=true;
+	private Model model=new Model();
+	@FXML
+	public void processNumber(ActionEvent event) {
+		if(start) {
+			result.setText("");
+			start=false;
+		}
+		String value=((Button)event.getSource()).getText();
+		result.setText(result.getText()+value);
+			
+	}
+	@FXML
+	public void processOperator(ActionEvent event) {
+
+		String value=((Button)event.getSource()).getText();
+		if(!value.equals("=")) {
+			if(!operator.isEmpty())
+				return;
+			operator=value;
+			number1=Long.parseLong(result.getText());
+			result.setText("");
+		}else {
+			if(operator.isEmpty())
+				return;
+			long number2=Long.parseLong(result.getText()); 
+			float outPut=model.calculate(number1, number2, operator);
+			result.setText(String.valueOf(outPut));
+			operator="";
+			start=true;
+		}
+	}
+}
